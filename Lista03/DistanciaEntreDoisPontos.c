f6351d15003417771bba23e4670165a06b003609
Rafael Schettino <63007993+rafaelschettino@users.noreply.github.com>
2020-08-26 09:17:38 -0300

    Update DistanciaEntreDoisPontos.c

--- Lista03/DistanciaEntreDoisPontos.c
+++ Lista03/DistanciaEntreDoisPontos.c
@@ -4,17 +4,24 @@
 
 int main()
 {
+    //Pares ordenados dos pontos a serem utilizados no calculo da distancia
     float x1;
     float y1;
     float x2;
     float y2;
-    float a;
+    //Variaveis auxiliares ao calculo
+    float a;    
     float a1;
-    float b;
+    float b;    
     float b1;
     float c;
+    //Resultado do calculo da distancia
     float d;
 
+    /**
+    *   d = sqrt( (x2 - x1)^2 + (y2 - y1)^2 )
+    /*
+    
     printf("Calculo da distancia entre dois pontos de uma reta\n");
     printf("Determine o valor de X2:\n");
     scanf("%f", &x2);
