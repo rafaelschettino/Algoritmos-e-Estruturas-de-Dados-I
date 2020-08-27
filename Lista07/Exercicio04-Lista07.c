39e3df53307fa80e6639367af9214b20b3cc39df
Rafael Schettino <63007993+rafaelschettino@users.noreply.github.com>
2020-08-27 10:06:15 -0300

    Update Exercicio04-Lista07.c

--- Lista07/Exercicio04-Lista07.c
+++ Lista07/Exercicio04-Lista07.c
@@ -8,20 +8,22 @@ int main()
     int a = 0;
     int b = 1;
     int f = a + b;
+    
     printf("Determine o numero de termos da sequencia\n");
     scanf("%d", &n);
+    
     printf("\nSequencia de Fibonacci\n");
     for(i = 1; i <= n; i++){
         if(i == 1){
             printf("%d\n", a);
-        } else if(i == 2){
+        }else if(i == 2){
             printf("%d\n", b);
-          }else{
+        }else{
             f = a + b;
             a = b;
             b = f;
             printf("%d\n", f);
-          }
-    }
+         }//fim if
+    }//fim for i
     return 0;
 }
