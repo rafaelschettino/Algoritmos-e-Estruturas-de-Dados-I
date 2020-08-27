b085c785aa118bde1ef8d4ac202fe3795a93adbf
Rafael Schettino <63007993+rafaelschettino@users.noreply.github.com>
2020-08-27 10:12:05 -0300

    Update Exercicio05-Lista07.c

--- Lista07/Exercicio05-Lista07.c
+++ Lista07/Exercicio05-Lista07.c
@@ -9,39 +9,37 @@ int main()
     float media;
     float soma = 0;
     float maior;
-    float flag = 0;
+    int flag = 0;
 
     printf("Determine a quantidade de valores que deseja utilizar:\n");
     scanf("%d", &n);
 
-        printf("\nDetermine os valores:\n");
-        printf("\nDigite [0], caso queira encerrar a leitura\n");
-        scanf("%d", &valor);
-        maior = valor;
-        soma = soma + valor;
-        if(valor == flag)
-            printf("\nPrograma encerrado!\n");
+    printf("\nDetermine os valores:\n");
+    printf("\nDigite [0], caso queira encerrar a leitura\n");
+    scanf("%d", &valor);
+    maior = valor;
+    soma = soma + valor;
+    if(valor == flag)
+        printf("\nPrograma encerrado!\n");
 
     while(valor != flag){
         for(c = 2; c <= n; c++){
             scanf("%d", &valor);
             if(valor > maior){
                 maior = valor;
-                }
+            }//fim if
             if(valor == flag){
                 printf("\nPrograma encerrado!\n");
                 break;
-            }
-                soma = soma + valor;
-        }
+            }//fim if
+            soma = soma + valor;
+        }//fim for c
         if(valor != flag){
             media = soma / n;
             printf("\nA media desses valores vale: %f", media);
             printf("\nO maior entre esses valores e: %f", maior);
-        }
-    }
-
-
+        }//fim if
+    }//fim while
 
     return 0;
 }
