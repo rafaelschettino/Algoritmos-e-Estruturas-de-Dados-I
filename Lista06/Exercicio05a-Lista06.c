10aff67aba7d6238dbd142d62246ba0e125308cd
Rafael Schettino <63007993+rafaelschettino@users.noreply.github.com>
2020-08-27 09:52:42 -0300

    Update Exercicio05a-Lista06.c

--- Lista06/Exercicio05a-Lista06.c
+++ Lista06/Exercicio05a-Lista06.c
@@ -3,32 +3,31 @@
 
 int main()
 {
-    int a;
-    char resp;
+    int ano;
+    char resp[10];
     do{
         printf("\nCalculo Ano Bissexto\n");
         printf("Informe o ano\n");
-        scanf("%d", &a);
-        if (a % 4 != 0) {
+        scanf("%d", &ano);
+        if (ano % 4 != 0) {
             printf("Ano nao bissexto\n");
     	}
-        else if (a % 4 == 0 && a % 100 != 0){
+        else if (ano % 4 == 0 && ano % 100 != 0){
             printf("Ano bissexto\n");
-        }else if (a % 4 == 0 && a % 100 == 0 && a % 400 != 0) {
+        }else if (ano % 4 == 0 && ano % 100 == 0 && ano % 400 != 0) {
             printf("Ano nao bissexto\n");
-	}else if (a % 4 == 0 && a % 100 == 0 && a % 400 == 0){
+	    }else if (ano % 4 == 0 && ano % 100 == 0 && ano % 400 == 0){
             printf("Ano bissexto\n");
     	}
         printf("\nVoce deseja testar um novo ano?\n");
         printf("Se SIM clique S");
         printf("\nSe NAO clique N\n");
         fflush(stdin);
-        scanf("%c", &resp);
-        if(resp == 'n' || resp == 'N'){
+        scanf("%s", resp);
+        if(resp[0] == 'n' || resp[0] == 'N'){
             printf("\nFim do programa!");
-            break;
-	}//fim if
-    }while(resp != 'N' || resp != 'n');
+	    }//fim if
+    }while(resp[0] == 'S' || resp[0] == 's');
 
     return 0;
 }
